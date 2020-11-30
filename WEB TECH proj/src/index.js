0ac8e0d1a2c1ec50147197974261116c6582f754
Janardhana440 <54240978+Janardhana440@users.noreply.github.com>
2020-11-30 21:57:53 +0530

    Add files via upload

---
+++ WEB TECH proj/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+<div class="" >
+      <img src="images/cart.png" alt="cart" class="nav-bar-img">
+      <a class="py-2 d-none d-md-inline-block">Cart</a>
+</div>,document.getElementById("n-bar"));
